657a02816f255d61252de96a208b3bd32d63c642
andresdigio <adigiovannimartinez@itba.edu.ar>
2020-12-19 20:42:14 -0300

    runner

--- src/analysis/runner.py
+++ src/analysis/runner.py
@@ -2,11 +2,11 @@ import os
 
 jar_path = 'C:/Users/Andres/ss-final/target/ss-final-1.0-jar-with-dependencies.jar'
 
-def run_simulation(ST, dt, o, path):
-    cmd = 'java -jar {:s} -data -ST {:g} -dt {:g} -o {:g} -out {:s}'.format(jar_path, ST, dt, o, path)
+def run_simulation(T, dt, o, path):
+    cmd = 'java -jar {:s} -data -T {:g} -dt {:g} -o {:g} -out {:s}'.format(jar_path, T, dt, o, path)
     print(cmd)
     os.system(cmd)
 
-for o in [0.5, 0.7, 0.9]:
+for o in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
     for i in [1, 2, 3, 4, 5]:
-        run_simulation(1000, 5e-5, o, '{:d}/{:d}.data'.format(int(o*100), i))
+        run_simulation(1500, 5e-5, o, '{:d}/{:d}.data'.format(int(o*100), i))
